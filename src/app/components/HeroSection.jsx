bc81eba497b88e6d0edeab445096a7fcb3128828
Aniket-404 <kamble.aniket.ak47@gmail.com>
2024-07-31 10:44:38 +0530

    Data Update

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -51,7 +51,7 @@ const HeroSection = () => {
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 to-purple-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download Resume
+                Download CV
               </span>
             </Link>
           </div>
